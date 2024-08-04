682097b7bb3451fd34454823fc48d14a7861b73c
Mohammed Nifli <mohammednifliap@gmail.com>
2024-08-04 22:06:28 +0530

    Tuple

---
+++ myTuple.ts
@@ -0,0 +1,16 @@
+
+//Tuple
+let tUser :[string,number,boolean];
+
+tUser=["k",2,true];
+
+
+//Type-Alias
+type User=[String,number] 
+let newUser:User=["l",7];
+
+//push method
+newUser[0]="hello";
+newUser.push("trUe")
+ 
+export{}
